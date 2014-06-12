902c7acd2113d69ca1485f702cffe71fdad3e3b6
Will Smidlein <will@willsmidlein.com>
2014-06-12 11:47:45 -0700

    V1!

--- README.md
+++ README.md
@@ -3,8 +3,31 @@ mockery
 
 Simulate SendGrid inbound webhook requests for testing.
 
-It's not really pretty right now, but it does the job.
+This is great if you're building something with the inbound webhook and don't want to have to manually send an email every time you want to test something.
 
+Options are configured via nconf, which means you can use CLI arguments, env variables, and/or a config.json file for configuration.
+
+## Notes
+
+Right now, it does not send valid **headers** or **spam_report** fields. It sends strings, but since I didn't feel like spending a year of my life writing fake header generation, they're just placeholder strings. If you feel like spending a year of your life writing fake header generation, a pull request would be much appreciated.
+
+It also sends different stuff for **text** and **html**, whereas you'd usually get the same content, but one in html form, and one in text form. This should be fixed soon, though I can't see how it would cause any major issues.
+
+## Options
+
+**url** (required): The URL you are testing
+
+**count** (optional, defaults to _1_): How many fake emails you want to send
+
+**boundary** (optional, defaults to _xYzZY_): If you don't know what this is, don't worry about it
+
+## Examples
+
+**Send 1 request**
+
+```
+node mockery.js --url="http://example.com/inbound"
+```
 
 **Send 5 requests**
 
--- mockery.js
+++ mockery.js
@@ -1,122 +1,27 @@
-var multiparter = require('multiparter')
-var http = require('http')
-var Faker = require('faker')
-var dimsum = require('dimsum')
-var config = require('nconf').argv().env().file({ file: 'config.json' });
+var Promise = require('bluebird')
+var config = require('nconf').argv().env().file({ file: 'config.json' })
+
+var Mockery = require('./lib/mockery.js')
+var fakeData = require('./lib/fakeData.js')
 
 config.defaults({
 	'count': 1,
 	'boundary': 'xYzZY'
 })
 
-var hitError = false;
+var mockery = new Mockery({
+	'url': config.get('url'),
+	'count': config.get('count'),
+	'boundary': config.get('boundary')
+})
 
-var opts = { 'url': config.get('url'), 'boundary': config.get('boundary') }
+var promises = [];
+for(var i = 0; i < config.get('count'); i++){ promises.push(mockery.makeRequest(fakeData.all())) }
 
-for(var i = 0; i < parseInt(config.get('count')) && hitError == false; i++){
-
-	var params = getParameters()
-
-	makeRequest(opts, params, function(err){
-		if(err){
-			console.log(err);
-			hitError = true;
-		}
-		else { console.log('Test ' + i + ' succeeded') }
+Promise.all(promises)
+	.then(function(){
+		console.log('Success!')
 	})
-
-}
-
-function makeRequest(options, params, cb){
-
-	var request = new multiparter.request(http, options);
-
-	for (var k in params){
-	    if (params.hasOwnProperty(k)) {
-	    	var v = params[k];
-	    	request.setParam(k, v);
-	    }
-	}
-
-	request.send(function(error, response) {
-
-		if(error){ reject(error); }
-		else {
-			var data = '';
-			response.setEncoding('utf8');
-
-			response.on('data', function(chunk) {
-				data += chunk;
-			})
-
-			response.on('end', function(chunk) {
-				cb(null);
-			})
-
-			response.on('error', function(err){
-
-			});
-		}
-
+	.catch(function(err){
+		console.log(err)
 	})
-
-}
-
-function fakeEmail(){
-	var email = Faker.Internet.email();
-	return {
-		email: email,
-		domain: email.split('@')[1]
-	}
-}
-
-function fakeBody(format, paragraphs){
-	return dimsum(paragraphs, { format: format });
-}
-
-http://www.xinotes.net/notes/note/341/
-function rPad(e,t,n){if(!e||!t||e.length>=n){return e}var r=(n-e.length)/t.length;for(var i=0;i<r;i++){e+=t}return e}
-
-function fakeSpamScore(){
-	var score = Math.round(Math.random() * 1000) / 1000 + '';
-	return rPad(score, '0', 5)
-}
-
-function randomInt(min, max){
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomBool(){
-	return (Math.random()<.5);
-}
-
-function getParameters(){
-
-	var params = {};
-
-	var from = fakeEmail();
-	var to = 'test@example.com';
-
-	params.to = to;
-	params.from = Faker.Name.findName() + ' <' + from.email + '>';
-	params.envelope = '{"to":["' + to + '"],"from":"' + from.email + '"}';
-	params.subject = Faker.Lorem.sentence();
-	params.dkim = '{@' + from.domain + ' : pass}';
-	params.spf = 'pass';
-	params.spam_score = fakeSpamScore();
-	params.spam_report = 'This isn\'t a real spam report.'; // TODO
-	params.sender_ip = Faker.Internet.ip();
-	params.attachments = '0'; // I don't feel like dealing with faking this right now.
-	params.headers = 'These aren\'t real headers.'; // I don't feel like dealing with faking this right now.
-	params.charsets = '{"to":"UTF-8","html":"UTF-8","subject":"UTF-8","from":"UTF-8","text":"UTF-8"}';
-
-	if(randomBool()){
-		params.text = fakeBody('text', randomInt(3, 7))
-	}
-	else {
-		params.html = fakeBody('html', randomInt(3, 7))
-	}
-
-	return params;
-
-}
